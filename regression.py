17c74d33d481f0613dff6c044f9b5912c93e72bd
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2020-03-22 06:58:42 -0700

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,7 +1,6 @@
 
 import json
 import numpy as np
-import urllib
 
 class Point(object):
   def __init__(self, x, y):
@@ -9,9 +8,9 @@ class Point(object):
     self.y = y
 
 class Parallelogram(object):
-  def __init__(self, id, pt1, pt2, pt3, pt4):
+  def __init__(self, id, p1, p2, p3, p4):
     """
-    A quadrilateral is defined by two lines: p1p2 and p1p3
+    A quadrilateral is defined by two lines: p2p1 and p2p3
     """
     self.id = id
     self.p1, self.p2, self.p3, self.p4 = p1, p2, p3, p4
@@ -23,18 +22,26 @@ class Parallelogram(object):
     return ((p1.x - p2.x)**2 + (p1.y - p2.y)**2)**0.5
 
   def find_angle(self, p1, p2, p3):
-    angle1 = np.arctan2(p3.y - p1.y, p3.x - p1.x)
-    angle2 = np.arctan2(p2.y - p1.y, p2.x - p1.x)
+    angle1 = np.arctan2(p3.y - p2.y, p3.x - p2.x)
+    angle2 = np.arctan2(p1.y - p2.y, p1.x - p2.x)
     return abs(angle1 - angle2)
 
-# def read_data(file):
-  
-#   parallelogram_arr = []
-#   for i in range(len(data)):
-#     id = data[i]['External ID']
-#     pts = data[i]['Label']['EXIT_sign'][0]['geometry']
-#     print(id)
-#     print(pts)
+def create_parallelograms(data):
+  parallelogram_arr = []
+  for i in range(len(data)):
+    id = data[i]['External ID']
+    try:
+      pts = data[i]['Label']['EXIT_sign'][0]['geometry']
+      p1 = Point(pts[0]['x'], pts[0]['y'])
+      p2 = Point(pts[1]['x'], pts[1]['y'])
+      p3 = Point(pts[2]['x'], pts[2]['y'])
+      p4 = Point(pts[3]['x'], pts[3]['y'])
+      parallelogram_arr.append(Parallelogram(id, p1, p2, p3, p4))
+    except:
+      continue
+  return parallelogram_arr
 
 with open('quadrilateral-1807.json') as f:
   data = json.load(f)
+parallelogram_arr = create_parallelograms(data)
+print(len(parallelogram_arr))
